e1ee696f6ac607ca551204d2054b48a01b23f5b6
xmisao <mail@xmisao.com>
2014-06-04 20:45:14 +0900

    batch.sh use new batch scripts

--- bin/batch.sh
+++ bin/batch.sh
@@ -1,12 +1,16 @@
 #!
 
+export RACK_ENV=development
 cd /home/admin/bestgems
-ruby lib/crawl.rb
-ruby lib/daily.rb
-ruby lib/total_rank.rb
-ruby lib/daily_rank.rb
-ruby lib/featured.rb
-ruby lib/stat_num_of_gems.rb
-ruby lib/stat_total_downloads.rb
-ruby lib/stat_daily_downloads.rb
-ruby lib/update_date.rb
+ruby lib/batch/scraping_all_gems.rb
+ruby lib/batch/update_gems.rb
+ruby lib/batch/update_total_downloads.rb
+ruby lib/batch/update_daily_downloads.rb
+ruby lib/batch/update_total_ranking.rb
+ruby lib/batch/update_daily_ranking.rb
+ruby lib/batch/update_featured_score.rb
+ruby lib/batch/update_featured_ranking.rb
+ruby lib/batch/update_statistics_num_of_gems.rb
+ruby lib/batch/update_statistics_total_downloads.rb
+ruby lib/batch/update_statistics_daily_downloads.rb
+ruby lib/batch/update_master.rb
